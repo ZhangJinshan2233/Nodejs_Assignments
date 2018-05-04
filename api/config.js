7f821da43b05d6af9c9ffb20629b006311a732d0
Zhang Jinshan <zhangjs2233@gmail.com>
2018-05-04 22:53:09 +0800

    change host name of mysql

--- api/config.js
+++ api/config.js
@@ -2,10 +2,10 @@ const config={
     //set up database
     database:{
         DATABASE:'heroku_8a9dfd8ed76ed69',
-        USERNAME:'us-cdbr-iron-east-04.cleardb.net',
+        USERNAME:'ba5300ad080b97',
         PASSWORD:'073b43d3',
         PORT:'3306',
-        HOST:'ba5300ad080b97'
+        HOST:'us-cdbr-iron-east-04.cleardb.net'
     }
     
 }
